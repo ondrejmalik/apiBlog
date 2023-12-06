6524bb1fc2b66ebdee10a236a043491676ad207f
o.m777@email.cz <malik@spsejecna.cz>
2023-12-06 10:01:06 +0100

    hotfix

--- api/obj/Release/net6.0/api.AssemblyInfo.cs
+++ api/obj/Release/net6.0/api.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bbd6ce2965af273c63f63c2353898b00b211730")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
